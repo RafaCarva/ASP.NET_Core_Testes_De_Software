b9d6fbe00e5f217f685463c60659c5dba6169ac0
Rafael Carvalho <rafael.faria.carvalho@gmail.com>
2019-12-29 12:57:18 -0300

    Exemplos de testes com XUnit

---
+++ 01-Testes-De-Unidade/Demo.Tests/AssertNullBoolTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Demo.Tests
+{
+    public class AssertNullBoolTests
+    {
+        [Fact]
+        public void Funcionario_Nome_NaoDeveSerNuloOuVazio()
+        {
+            // Arrange & Act
+            var funcionario = new Funcionario("", 1000);
+
+            // Assert
+            Assert.False(string.IsNullOrEmpty(funcionario.Nome)); // Veja o construtor de Funcionario, esse nome nunca será vazio
+        }
+
+        [Fact]
+        public void Funcionario_Apelido_NaoDeveTerApelido()
+        {
+            // Arrange & Act
+            var funcionario = new Funcionario("Rafael", 1000);
+
+            // Assert
+            Assert.Null(funcionario.Apelido);
+            // Assert
+            Assert.True(string.IsNullOrEmpty(funcionario.Apelido));
+            Assert.False(funcionario.Apelido?.Length > 0);
+        }
+    }
+}
---
+++ 01-Testes-De-Unidade/Demo.Tests/AssertStringsTests.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Demo.Tests
+{
+    public class AssertStringsTests
+    {
+        [Fact]
+        public void StringsTools_UnirNomes_RetornarNomeCompleto()
+        {
+            // Arrange
+            var sut = new StringsTools();
+
+            // Act
+            var nomeCompleto = sut.Unir("Rafael", "Carvalho");
+
+            // Assert
+            Assert.Equal("Rafael Carvalho", nomeCompleto);
+        }
+
+        [Fact]
+        public void StringsTools_UnirNomes_DeveIgnorarCase()
+        {
+            // Arrange
+            var sut = new StringsTools();
+
+            // Act
+            var nomeCompleto = sut.Unir("Rafael", "Carvalho");
+
+            // Assert
+            Assert.Equal("RAFAEL CARVALHO", nomeCompleto, true); // O 5º overload mostra esse bool como 3º parâmetro
+        }
+
+        [Fact]
+        public void StringsTools_UnirNomes_DeveConterTrecho()
+        {
+            // Arrange
+            var sut = new StringsTools();
+
+            // Act
+            var nomeCompleto = sut.Unir("Rafael", "Carvalho");
+
+            // Assert
+            Assert.Contains("alho", nomeCompleto);
+        }
+
+        [Fact]
+        public void StringsTools_UnirNomes_DeveComecarCom()
+        {
+            // Arrange
+            var sut = new StringsTools();
+
+            // Act
+            var nomeCompleto = sut.Unir("Rafael", "Carvalho");
+
+            // Assert
+            Assert.StartsWith("Raf", nomeCompleto);
+        }
+
+        [Fact]
+        public void StringsTools_UnirNomes_DeveAcabarCom()
+        {
+            // Arrange
+            var sut = new StringsTools();
+
+            // Act
+            var nomeCompleto = sut.Unir("Rafael", "Carvalho");
+
+            // Assert
+            Assert.EndsWith("lho", nomeCompleto);
+        }
+
+        [Fact]
+        public void StringsTools_UnirNomes_ValidarExpressaoRegular()
+        {
+            // Arrange
+            var sut = new StringsTools();
+
+            // Act
+            var nomeCompleto = sut.Unir("Rafael", "Carvalho");
+
+            // Assert
+            Assert.Matches("[A-Z]{1}[a-z]+ [A-Z]{1}[a-z]+", nomeCompleto);
+        }
+    }
+}
